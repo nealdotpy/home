2b105f0fe864c1e4e1e3064c847a42cb93624697
nealpatel <nealpatel2021@gmail.com>
2020-07-01 17:02:08 -0400

    Updates

---
+++ precache-manifest.b50981bf9b688cb9fb6209bd76a6753b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59dba9f598512163cff604dc76c704fa",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "2b5bc1398b77a8e72ce0",
+    "url": "/home/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "b590cd51808ba56f3f43",
+    "url": "/home/static/css/main.a82b2030.chunk.css"
+  },
+  {
+    "revision": "2b5bc1398b77a8e72ce0",
+    "url": "/home/static/js/2.a642fdd8.chunk.js"
+  },
+  {
+    "revision": "ec0e426974e24df94e250935d5ec02a2",
+    "url": "/home/static/js/2.a642fdd8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b590cd51808ba56f3f43",
+    "url": "/home/static/js/main.a6fad627.chunk.js"
+  },
+  {
+    "revision": "5c07ecca0456e28e04f0",
+    "url": "/home/static/js/runtime-main.e084aacb.js"
+  },
+  {
+    "revision": "aa590f0eedfddaeb0c534cc55f894e7e",
+    "url": "/home/static/media/me2160.aa590f0e.png"
+  },
+  {
+    "revision": "fcf4e9cd0b88b791143d07e2344297c7",
+    "url": "/home/static/media/ml_cam_pic.fcf4e9cd.jpg"
+  },
+  {
+    "revision": "01a0c8dbced83afaef0cc22c81d2e01c",
+    "url": "/home/static/media/resume.01a0c8db.pdf"
+  },
+  {
+    "revision": "75b4644e7658ade9b0b630c562735bd1",
+    "url": "/home/static/media/trade_bot_pic.75b4644e.jpg"
+  }
+]);
